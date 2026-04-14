d7a5a403a076bda2fc728a12b24ba0a73d11a1b9
SADIK YILDIZ <117443290+sadikyildiz@users.noreply.github.com>
2026-04-14 14:24:57 +0300

    Add files via upload

---
+++ PersonelSqlEkleme1.sql
@@ -0,0 +1,2 @@
+INSERT INTO personel(ad_soyad,cinsiyet,departman,giris_tarihi,maas,eposta)
+value("Gizem A.","K","İnsan Kaynakları","2020-11-23",8000,"gizem@ornek.com")
---
+++ PersonelSqlEkleme2.sql
@@ -0,0 +1,9 @@
+INSERT INTO personel(ad_soyad, cinsiyet, departman, giris_tarihi, maas, eposta)
+VALUES ("Arzu Y.", "K", "Satış", "2021-10-18",7500, "arzu@ornek.com"),
+("Merve K.", "K", "Satış", "2021-10-18", 7000, "merve@deneme.com"),
+("Murat Z.","E", "Güvenlik", "2019-01-20", 7000, "muratz@ornek.com"),
+("Turan S.", "E", "Bilişim Teknolojileri", "2017-01-05",15000, "turan@deneme.com"),
+("Faruk P.","E", "Üretim", "2019-02-22", 8000, "faruk@ornek.com"),
+("Mustafa S.", "E", "Üretim", "2018-07-11",8500, "mustafa@deneme.com"),
+("Yasin B.","E", "Bilişim Teknolojileri", "2017-03-06", 11500, "yasin@deneme.com"),
+("Yasemin A.", "K", "Bilişim Teknolojileri","2019-06-08",10500, "yasemin@deneme.com")
