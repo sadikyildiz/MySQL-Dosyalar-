494f5567a5e1df66e714d3fb369f3423efa3ce67
SADIK YILDIZ <117443290+sadikyildiz@users.noreply.github.com>
2026-04-14 14:49:38 +0300

    Add files via upload

---
+++ PersonelSqlEkleme3.sql
@@ -0,0 +1,6 @@
+INSERT INTO musteri(ad_soyad,cinsiyet,adres,dogum_tarihi,telefon,yasadıgı_sehir)
+value("Demir Z.","K","100. sokak no:20","2000-07-07","318 79 96","Ankara"),
+("Ali B.","E","4500. sokak no:5/12","1995-02-21","218 50 20","İzmir"),
+("Burak K.","E","1923. sokak no:56","1986-05-31","350 60 90","Ankara"),
+("Gül Ş.","K","10 cad. 1786. sokak no:20","2000-07-07","469 60 41","İstanbul"),
+("Feride Y.","K","6589. sokak no:62/19","2002-08-22","220 41 97","Ankara")
